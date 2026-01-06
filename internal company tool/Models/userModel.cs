141227b71d2167b79e5697f29738dac6c959c8fd
Joshua Dean Shields <shields.joshua23@gmail.com>
2026-01-06 13:52:34 +0200

    added to user model getters and setters

--- internal company tool/Models/userModel.cs
+++ internal company tool/Models/userModel.cs
@@ -2,5 +2,24 @@ namespace CRUD_APP_.Models;
 
 public class User_model
 {
-    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+}
+
+public class userloginmodel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+public class User_ticket_model
+{
+    public int TicketId { get; set; }
+    public int UserId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Status { get; set; }
 }
