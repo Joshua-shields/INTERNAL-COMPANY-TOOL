12526de3b1126606715d9dd140d1cac5585e6243
Joshua Dean Shields <shields.joshua23@gmail.com>
2026-01-06 12:11:29 +0200

    built the base now the building starts

---
+++ internal company tool/Models/Adminhomedashboard.cs
@@ -0,0 +1,12 @@
+namespace CRUD_APP_.Models;
+
+public class Adminhomedashboard
+{
+    public int TotalUsers { get; set; }
+    public int ActiveUsers { get; set; }
+    public int InactiveUsers { get; set; }
+    public int TotalRoles { get; set; }
+    public string AdminName { get; set; }
+    public string AdminEmail { get; set; }
+    
+}
